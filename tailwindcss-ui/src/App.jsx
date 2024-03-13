c714bf62609a4852eeee5c92c657105ee0b52139
Henrique Dourado Domingos <henriquedsenai@gmail.com>
2024-03-12 21:20:42 -0300

    commit

---
+++ tailwindcss-ui/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='h-screen bg-zinc-100 dark:bg-zinc-800 dark:text-zinc-100 text-zinc-50 grid place-content-center'>
+     <h1 className='text-3xl text-center'>Ready to start with Tailwind!</h1>
+     <button className='block w-fit mx-auto my-8 px-4 py-2 rounded-lg 
+     border-transparent hover:border-cyan-500 border-2 transition-colors'
+     onClick={() => setCount(count => count + 1)}
+     >
+      count is {count}
+     </button>
+    </div>
+  )
+}
+
+export default App
